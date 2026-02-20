72325a920ea0ecffa254ce38231c53a99296ee74
xtosis <33601730+xtosis@users.noreply.github.com>
2026-02-20 19:30:24 +0000

    run-sweep | ARTWORKS version

--- run-sweep.py
+++ run-sweep.py
@@ -1,12 +1,13 @@
+import os
 from sweep.sweep import sweep, load_json_data
 
 SIM_NAME = ""
-SIM_CONFIG = ""
 
-ARTWORK = ""
+ARTWORKS = ""
+
+SIM_CONFIG = ""
 SWEEP = ""
 
-OUTPUT_DIR = ""
 OUTPUT_NAME = ""
 
 GENERATE_LAYOUT = True
@@ -14,18 +15,20 @@ GENERATE_SVG = True
 RUN_SIM = True
 PACK = True
 
-sweep(
-    SIM_NAME,
-    load_json_data(ARTWORK),
-    load_json_data(SWEEP),
-    SIM_CONFIG,
-    OUTPUT_DIR,
-    OUTPUT_NAME,
-    GENERATE_LAYOUT,
-    GENERATE_SVG,
-    RUN_SIM,
-    PACK,
-    force=False,
-    verbose=False,
-    log_level=None
-)
+for artwork in os.listdir(ARTWORKS):
+    output_dir = ""
+    sweep(
+        SIM_NAME,
+        load_json_data(f'{ARTWORKS}/{artwork}'),
+        load_json_data(SWEEP),
+        load_json_data(SIM_CONFIG),
+        output_dir,
+        OUTPUT_NAME,
+        GENERATE_LAYOUT,
+        GENERATE_SVG,
+        RUN_SIM,
+        PACK,
+        force=False,
+        verbose=False,
+        log_level=None
+    )
