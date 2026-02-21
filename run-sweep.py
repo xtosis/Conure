17bb2d97aca655170653aef5648b82c6203918c2
xtosis <33601730+xtosis@users.noreply.github.com>
2026-02-21 11:31:43 +0000

    run-sweep | std directory structure

--- run-sweep.py
+++ run-sweep.py
@@ -1,14 +1,27 @@
 import os
 from sweep.sweep import sweep, load_json_data
-from configs.run_sweep import *
 
-for artwork in os.listdir(ARTWORKS):
+# --- Main Input ------------
+JOB_NAME = "01-sweep"
+# ---------------------------
+
+SIM_NAME = "emx" # fixed cannot change
+GENERATE_LAYOUT = True
+GENERATE_SVG = True
+RUN_SIM = True
+PACK = True
+
+_JOB_PATH = "/mnt/storage/shared/jobs/" + JOB_NAME
+_CONFIGS = _JOB_PATH + "/_CONFIGS/"
+_ARTWORKS = _CONFIGS + "_ARTWORKS"
+
+for artwork in os.listdir(_ARTWORKS):
     sweep(
         SIM_NAME,
-        load_json_data(f'{ARTWORKS}/{artwork}'),
-        load_json_data(SWEEP),
-        SIM_CONFIG,
-        f"output/{artwork.replace(".json", "")}",
+        load_json_data(f'{_ARTWORKS}/{artwork}'),
+        load_json_data(_CONFIGS + "sweep.json"),
+        _CONFIGS + "sim.json",
+        f"{_JOB_PATH}/output/{artwork.replace(".json", "")}",
         None,
         GENERATE_LAYOUT,
         GENERATE_SVG,
